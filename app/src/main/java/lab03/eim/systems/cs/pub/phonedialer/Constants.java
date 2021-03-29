9fed0d1febc6c3f01606a3136d28ce79081d7faa
Andrei Sosea <sosea.andrei@yahoo.com>
2021-03-29 10:14:41 +0300

    Initital empty project

---
+++ app/src/main/java/lab03/eim/systems/cs/pub/phonedialer/Constants.java
@@ -0,0 +1,6 @@
+package lab03.eim.systems.cs.pub.phonedialer;
+
+public class Constants {
+
+    final public static int PERMISSION_REQUEST_CALL_PHONE = 1;
+}
---
+++ app/src/main/java/lab03/eim/systems/cs/pub/phonedialer/MainActivity.java
@@ -0,0 +1,108 @@
+package lab03.eim.systems.cs.pub.phonedialer;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText phoneNumberEditText;
+    private ImageButton callImageButton;
+    private ImageButton hangupImageButton;
+    private ImageButton backspaceImageButton;
+    private Button genericButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_dialer);
+
+        phoneNumberEditText = (EditText)findViewById(R.id.phone_number_edit_text);
+        callImageButton = (ImageButton)findViewById(R.id.call_image_button);
+        callImageButton.setOnClickListener(callImageButtonClickListener);
+        hangupImageButton = (ImageButton)findViewById(R.id.hangup_image_button);
+        hangupImageButton.setOnClickListener(hangupImageButtonClickListener);
+        backspaceImageButton = (ImageButton)findViewById(R.id.backspace_image_button);
+        backspaceImageButton.setOnClickListener(backspaceButtonClickListener);
+
+        genericButton = (Button)findViewById(R.id.number_0_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_1_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_2_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_3_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_4_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_5_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_6_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_7_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_8_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_9_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.star_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.number_0_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+        genericButton = (Button)findViewById(R.id.pound_button);
+        genericButton.setOnClickListener(genericButtonClickListener);
+    }
+
+    private CallImageButtonClickListener callImageButtonClickListener = new CallImageButtonClickListener();
+    private class CallImageButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        Constants.PERMISSION_REQUEST_CALL_PHONE);
+            } else {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + phoneNumberEditText.getText().toString()));
+                startActivity(intent);
+            }
+        }
+    }
+
+    private HangupImageButtonClickListener hangupImageButtonClickListener = new HangupImageButtonClickListener();
+    private class HangupImageButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    }
+
+    private BackspaceButtonClickListener backspaceButtonClickListener = new BackspaceButtonClickListener();
+    private class BackspaceButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String phoneNumber = phoneNumberEditText.getText().toString();
+            if (phoneNumber.length() > 0) {
+                phoneNumber = phoneNumber.substring(0, phoneNumber.length() - 1);
+                phoneNumberEditText.setText(phoneNumber);
+            }
+        }
+    }
+
+    private GenericButtonClickListener genericButtonClickListener = new GenericButtonClickListener();
+    private class GenericButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            phoneNumberEditText.setText(phoneNumberEditText.getText().toString() + ((Button)view).getText().toString());
+        }
+    }
+}
